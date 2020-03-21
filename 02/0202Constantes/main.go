5625f7e5207f2582ba75196bc25700de590ad90d
Pedro Luis <luistyle03@gmail.com>
2020-03-20 19:22:43 -0500

    Agreganado 0201 Variables y constantes comentado

---
+++ 02/0202Constantes/main.go
@@ -0,0 +1,21 @@
+//Comentario de Linea: sirve para
+//documentar el
+//codigo
+
+/*
+Comentario de bloque: para comentar codigo o comentar al inicio sobre lo que hace un paquete
+var a int
+a=8
+*/
+package main
+
+import "fmt"
+
+func main() {
+	const nombre = "Pedro"
+
+	//no se puede cambiar el valor de una constante
+	//nombre = "Juan"
+
+	fmt.Println(nombre)
+}
